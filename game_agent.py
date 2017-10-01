ccd57f6bf86309dae327f0be399dc3dcbd3fad1e
Jose Luis Matus <jmatus@annwynsoft.com>
2017-10-01 10:01:13 -0600

    Implemented custom scores

--- game_agent.py
+++ game_agent.py
@@ -35,7 +35,28 @@ def custom_score(game, player):
         The heuristic value of the current game state to the specified player.
     """
     # TODO: finish this function!
-    raise NotImplementedError
+
+    if game.is_loser(player):
+        return float("-inf")
+
+    if game.is_winner(player):
+        return float("inf")
+
+
+    own_moves = len(game.get_legal_moves(player))
+    opp_moves = len(game.get_legal_moves(game.get_opponent(player)))
+    value = float(own_moves - opp_moves)
+       
+    rows = [r for r in range(game.width)]
+    cols = [c for c in range(game.height)]  
+    borders = [(0,c) for c in cols]
+    borders = borders + [(game.width - 1,c) for c in cols]
+    borders = borders + [(r,0) for r in rows]
+    borders = borders + [(r,game.height - 1) for r in rows]
+    if game.get_player_location(player) in  borders: 
+        return value - 2
+
+    return value
 
 
 def custom_score_2(game, player):
@@ -60,8 +81,20 @@ def custom_score_2(game, player):
     float
         The heuristic value of the current game state to the specified player.
     """
-    # TODO: finish this function!
-    raise NotImplementedError
+    if game.is_loser(player):
+        return float("-inf")
+
+    if game.is_winner(player):
+        return float("inf")
+
+    own_moves = len(game.get_legal_moves(player))
+    opp_moves = len(game.get_legal_moves(game.get_opponent(player)))
+    value = float(own_moves - opp_moves)
+    
+    if game.get_player_location(player) in  [(0, 0), (0, game.height - 1), (game.width - 1, 0), (game.width - 1, game.height - 1)]: # in the corner so we should penalize 
+        return value - 2
+    
+    return value
 
 
 def custom_score_3(game, player):
@@ -87,8 +120,31 @@ def custom_score_3(game, player):
         The heuristic value of the current game state to the specified player.
     """
     # TODO: finish this function!
-    raise NotImplementedError
+    if game.is_loser(player):
+        return float("-inf")
 
+    if game.is_winner(player):
+        return float("inf")
+
+
+    own_moves = len(game.get_legal_moves(player))
+    opp_moves = len(game.get_legal_moves(game.get_opponent(player)))
+    value = float(own_moves - opp_moves)
+       
+    rows = [r for r in range(game.width)]
+    cols = [c for c in range(game.height)]  
+    borders = [(0,c) for c in cols]
+    borders = borders + [(game.width - 1,c) for c in cols]
+    borders = borders + [(r,0) for r in rows]
+    borders = borders + [(r,game.height - 1) for r in rows]
+
+    if game.get_player_location(player) in  [(0, 0), (0, game.height - 1), (game.width - 1, 0), (game.width - 1, game.height - 1)]: # in the corner so we should penalize 
+        return value - 2
+
+    if game.get_player_location(player) in  borders: 
+        return value - 1
+
+    return value
 
 class IsolationPlayer:
     """Base class for minimax and alphabeta agents -- this class is never
